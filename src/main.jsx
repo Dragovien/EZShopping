fc3e68384729ac0d70e0d16a198e8f656abf7072
Axel Hégron <axel.hegron@3wa.io>
2023-06-20 16:39:04 +0200

    added Router on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './pages/App'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './stores/globalStore'
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
 )
