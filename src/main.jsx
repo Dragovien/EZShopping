990075bb8fd2e8d539d3abfc1514823b176323fb
Jo-han Cruellas <cruellasjohan@gmail.com>
2023-06-20 16:47:11 +0200

    remove boilerplate

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/App'
 import User from './pages/UserPage'
 import Cart from './pages/CartPage'
 import Products from './pages/ProductsPage'
