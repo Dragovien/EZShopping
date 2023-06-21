92e0d2f7893c5780f0dd649bed5484ab2cc76a4b
Jo-han Cruellas <cruellasjohan@gmail.com>
2023-06-21 11:13:00 +0200

    productCard WIP missing addToCart handle

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductCard = ({ title, description, category, price,imageSrc }) => {
+const ProductCard = ({ title, description, category, price, imageSrc }) => {
+
+  const [quantity, setQuantity] = useState(1);
+
+  const quantityHandler = (e) => {
+    setQuantity(e.target.value);
+  }
+
   return (
     <div className="productCard">
-      <img src={imageSrc} alt={title}/>
-      <div className="cardTextWrapper">
-        <h2 className="cutText">{title}</h2>
-        <div className="categoryChip">{category}</div>
-        <div className="cutText">{description}</div>
-        <h3>{price}$</h3>
+      <div className='cardContent'>
+        <img src={imageSrc} alt={title} />
+        <div className="cardTextWrapper">
+          <h2 className="cutText">{title}</h2>
+          <div className="categoryChip">{category}</div>
+          <div className="cutText">{description}</div>
+          <h3>{price}$</h3>
+        </div>
+      </div>
+      <div className="cardFooter">
+        Quantity : <input value={quantity} onChange={quantityHandler}></input>
+        <button className="orangeButton">Add to cart</button>
       </div>
     </div>
   );
